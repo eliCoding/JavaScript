6cf49a8d55801188f939f5ae0490810c14fb259a
Elmira Amanollahi <e.amanollahi@gmail.com>
2016-12-19 15:38:00 -0500

    Lab7

---
+++ Lab7/JQuary/scripts/jsloaded.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+if ($) {
+    $(document).ready(
+        function () {
+            $('p').addClass('FrameworkLoaded');
+            $('p').text('jQuery successfully loaded and runing!');
+        }//end function
+        );
+}//end outer if
