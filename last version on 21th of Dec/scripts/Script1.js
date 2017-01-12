5eaade79d5870109665d32b2ff2586abe295d63d
Elmira Amanollahi <e.amanollahi@gmail.com>
2017-01-11 20:04:10 -0500

    Project

---
+++ last version on 21th of Dec/scripts/Script1.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+// JavaScript source code
+
+// create function today() that returns the current date
+//in the format mm-dd-yyyy
+
+function today() {
+    var now = new Date();
+    //extract from the now object thed ae m month ,and full year
+    var thisDate = now.getDate();
+    var thisMonth = now.getMonth() + 1;
+    var thisYear = now.getFullYear();
+
+    var currentDate = thisMonth + "-" + thisDate + "-" + thisYear;
+    return currentDate;
+}
